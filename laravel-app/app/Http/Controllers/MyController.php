2cd9f083270201a225e85674e712de03cd9c6f90
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-29 21:59:42 +0300

    Added Sorting Function

--- laravel-app/app/Http/Controllers/MyController.php
+++ laravel-app/app/Http/Controllers/MyController.php
@@ -6,7 +6,20 @@ use Illuminate\Http\Request;
 
 class MyController extends Controller {
     
-    function sortedString() {
-        return "Test Complete";
+    function sortedString($myStr) {
+        $arr = [];
+        $sortedString = "";
+
+        foreach (str_split($myStr) as $l) {
+            $arr[] = $l;
+        }
+
+        sort($arr);
+
+        foreach ($arr as $i) {
+            $sortedString .= $i;
+        }
+
+        return $sortedString;
     }
 }
--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -4,8 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
 
-Route::get("/", [MyController::class, "sortedString"]);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get("/{myStr}", [MyController::class, "sortedString"]);
