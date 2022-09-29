d35c98d881533bc96b89c4d4ca8ab465c800b368
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-29 20:54:53 +0300

    Linked MyController With Routes

---
+++ laravel-app/app/Http/Controllers/MyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyController extends Controller {
+    
+    function sortedString() {
+        return "Test Complete";
+    }
+}
