28d49049eef33b9ac77937f65be93fbe8edf58cd
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-29 22:48:36 +0300

    Finished Sort String Algo

--- laravel-app/app/Http/Controllers/MyController.php
+++ laravel-app/app/Http/Controllers/MyController.php
@@ -7,17 +7,44 @@ use Illuminate\Http\Request;
 class MyController extends Controller {
     
     function sortedString($myStr) {
-        $arr = [];
+        $arrSmall = [];
+        $arrCap = [];
+        $arrNum = [];
         $sortedString = "";
 
         foreach (str_split($myStr) as $l) {
-            $arr[] = $l;
+            if ($l >= "A" and $l <= "Z") {
+                $arrCap[] = $l;
+            } else if ($l >= "a" and $l <= "z"){
+                $arrSmall[] = $l;
+            } else {
+                $arrNum[] = $l;
+            }
         }
 
-        sort($arr);
+        sort($arrSmall);
+        sort($arrCap);
+        sort($arrNum);
 
-        foreach ($arr as $i) {
-            $sortedString .= $i;
+        for ($i = 0; $i < count($arrSmall); $i++) {
+            if ($arrCap) {
+                if ($arrSmall[$i] > strtolower($arrCap[0])) {
+                    $sortedString .= $arrCap[0];
+                    unset($arrCap[0]);
+                    $arrCap = array_values($arrCap);
+                }
+            }
+            $sortedString .= $arrSmall[$i];
+        }
+
+        if ($arrCap) {
+            foreach ($arrCap as $cap) {
+                $sortedString .= $cap;
+            }
+        }
+
+        foreach ($arrNum as $num) {
+            $sortedString .= $num;
         }
 
         return $sortedString;
--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
 
-Route::get("/{myStr}", [MyController::class, "sortedString"]);
+Route::get("/sort_string/{myStr}", [MyController::class, "sortedString"]);
